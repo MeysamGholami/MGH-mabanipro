093f001939cb5effb5fe760acdd106f85c268f34
MeysamGholami <mysm.qolami@gmail.com>
2022-08-18 22:58:01 +0430

    edit js

--- main.js
+++ main.js
@@ -3,9 +3,20 @@ var dot = document.getElementsByClassName('dot');
 var prev = document.querySelector('.prev');
 var next = document.querySelector('.next');
 var n = 0;
+var i;
+
+function disno(){
+    for(i = 0 ; i<item.length ; i++){
+        item[i].style.display = 'none';
+    }
+}
 
 next.addEventListener('click',function(e){
     e.preventDefault();
     n++;
+    if(n > item.length - 1){
+        n = 0;
+    }
+    disno();
     item[n].style.display = 'block';
 })
