3b2d70d1ae896281e1c96bc14519ba730e7ed915
MeysamGholami <mysm.qolami@gmail.com>
2022-08-18 23:20:21 +0430

    move js file to folder

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var item = document.getElementsByClassName('item');
+var dot = document.getElementsByClassName('dot');
+var prev = document.querySelector('.prev');
+var next = document.querySelector('.next');
+var n = 0;
+var i;
+
+function disno(){
+    for(i = 0 ; i<item.length ; i++){
+        item[i].style.display = 'none';
+    }
+}
+
+function no_active(){
+    for( i = 0 ; i<dot.length ; i++){
+        dot[i].classList.remove('active');
+    }
+}
+
+next.addEventListener('click',function(e){
+    e.preventDefault();
+    n++;
+    if(n > item.length - 1){
+        n = 0;
+    }
+    disno();
+    no_active();
+    item[n].style.display = 'block';
+    dot[n].classList.add('active');
+})
+
+prev.addEventListener('click',function(e){
+    e.preventDefault();
+    n--;
+    if(n < 0){
+        n = item.length - 1;
+    }
+    disno();
+    no_active();
+    item[n].style.display = 'block';
+    dot[n].classList.add('active');
+})
